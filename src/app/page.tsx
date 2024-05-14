cdb0d901122cd3f2d114329a2888ac0b8079ab31
Michael Carter <michaelcarter@Michaels-Air.JOOWIN>
2024-05-14 09:16:03 -0400

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/8e60682f-303b-44f6-8bdd-aadc4189247c-xxe3v5.jpeg",
+  "https://utfs.io/f/b67245cd-627c-4776-ae1e-a9cc029e4c6d-1xb6v1.jpeg"
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48 p-4">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
