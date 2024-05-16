874a64c8ccfb5b001bddacfe0d9db57a1d3d18b9
Michael Carter <michaelcarter@Michaels-Air.JOOWIN>
2024-05-15 20:17:25 -0400

    disable caching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic"
 const mockUrls = [
   "https://utfs.io/f/8e60682f-303b-44f6-8bdd-aadc4189247c-xxe3v5.jpeg",
   "https://utfs.io/f/b67245cd-627c-4776-ae1e-a9cc029e4c6d-1xb6v1.jpeg"
